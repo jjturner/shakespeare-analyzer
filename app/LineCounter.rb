129f9e9756d1088deb97f5a8bb13488218a89a43
John Turner <fenwayriffs@gmail.com>
2016-01-23 23:32:22 -0500

    passes 2nd test - aggregate lines for each speech

--- app/LineCounter.rb
+++ app/LineCounter.rb
@@ -5,7 +5,8 @@ class LineCounter
   def lines(doc)
     # XPath is 1-base, but Nokogiri is 0-base
     character = doc.xpath("//SPEAKER")[0].text
-    line_count = doc.xpath("//SPEECH").children.count - 1
+    speech_count = doc.xpath("//SPEECH").count
+    line_count = doc.xpath("//SPEECH").children.count - speech_count
     "%d %s" % [line_count, character]
   end
 end
--- spec/list_speaker_line_count_spec.rb
+++ spec/list_speaker_line_count_spec.rb
@@ -17,4 +17,22 @@ describe LineCounter do
     expect(character_lines).to eq "2 Bob"
     
   end
+
+  it "aggregates counts for each speech" do
+    # setup
+    xml = "<PLAY><SPEECH><SPEAKER>Bob</SPEAKER>"
+    xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH>"
+    # xml += "<PLAY><SPEECH><SPEAKER>Fred</SPEAKER>"
+    # xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH></PLAY>"
+    xml += "<SPEECH><SPEAKER>Bob</SPEAKER>"
+    xml += "<LINE>Hello</LINE><LINE>Bye</LINE></SPEECH></PLAY>"
+    doc = Nokogiri::XML(xml)
+    line_counter = LineCounter.new
+
+    # exercise
+    character_lines = line_counter.lines(doc)
+
+    # verify
+    expect(character_lines).to eq "4 Bob"
+  end
 end
