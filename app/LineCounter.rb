93b597b576b4d92c079d339041e5177520c21549
John Turner <fenwayriffs@gmail.com>
2016-03-03 00:57:57 -0500

    refactored analyzer by eliminating extended method for sums

--- app/LineCounter.rb
+++ app/LineCounter.rb
@@ -16,34 +16,37 @@ class LineCounter
   end
 
   def analyze_macbeth
-    prelim = count_lines_per_speech
-    result_hash = sum_lines_by_speaker(prelim)
-    puts display(result_hash)
+    list = sum_lines_by_speaker
+    # result_hash = sum_lines_by_speaker(prelim)
+    puts display(list)
   end
 
-  def count_lines_per_speech
+  def sum_lines_by_speaker
     # XPath is 1-base, but Nokogiri is 0-base
     # character = doc.xpath("//SPEAKER")[0].text
     speeches = @doc.xpath("//SPEECH[not(SPEAKER='ALL')]")
-    prelim = speeches.inject([]) do |agg, speech|
-      line_count = speech.xpath("./LINE").count
-      character = speech.xpath("./SPEAKER").text
-      agg << { character => line_count }
-    end
-  end
-
-  def sum_lines_by_speaker(hash_array)
-
-    result_hash = Hash.new { |hash, key| hash[key] = 0 }
-
-    hash_array.each do |e|
-      e.each do |k,v|
-        result_hash[k] += v
+    list = speeches.inject(Hash.new(0)) do |agg, speech|
+      character = speech.xpath("./SPEAKER").text.to_s
+      lines = speech.xpath("./LINE")
+      lines.each do |line|
+        agg[character] += 1 
       end
+      agg
     end
-
-    return result_hash
   end
+
+#  def sum_lines_by_speaker(hash_array)
+#
+#    result_hash = Hash.new { |hash, key| hash[key] = 0 }
+#
+#    hash_array.each do |e|
+#      e.each do |k,v|
+#        result_hash[k] += v
+#      end
+#    end
+#
+#    return result_hash
+#  end
 
   def display(hash)
     output = hash.sort_by{|k,v|-v}.collect {|k,v| "#{v} #{k}"}.join("\n")
